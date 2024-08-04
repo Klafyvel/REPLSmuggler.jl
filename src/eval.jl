772f3d8b26b7958607da424456ffad9eba7b97ef
qubitnerd <qubitnerd>
2024-08-05 00:27:53 +0530

    evaluate_entry renamed to evaluate_entry_old
    new function evaluate_entry added which instead has the following behaviour
    multilines sendt to the repl are not executed line by line , instead are executed as a single block

--- src/eval.jl
+++ src/eval.jl
@@ -12,7 +12,7 @@ context of the code with `file` and `line`. If an error occurs, it will put a
 using Base: JuliaSyntax
 [`Protocols.Error`](@ref) to the outgoing channel of the session.
 """
-function evaluate_entry(session, msgid, file, line, value)
+function evaluate_entry_old(session, msgid, file, line, value)
     @debug "Evaluating entry" session file line value
     repl = Base.active_repl
     current_line = line
@@ -90,4 +90,46 @@ function evaluate_entry(session, msgid, file, line, value)
         write(REPL.LineEdit.terminal(s), take!(io))
         flush(REPL.LineEdit.terminal(s))
     end
+end
+
+function evaluate_entry(session, msgid, file, line, value)
+    @debug "Evaluating entry" session file line value
+    repl = Base.active_repl
+    current_line = line
+    s = repl.mistate.mode_state[repl.mistate.current_mode]
+    eval_string = value
+    @debug "Printing eval string" eval_string
+    REPL.LineEdit.edit_insert(s,eval_string)
+    REPL.LineEdit.commit_line(repl.mistate)
+
+    expr = Meta.parseall(eval_string)
+    @debug "expr before adjustment" expr
+    for node in AbstractTrees.PostOrderDFS(expr)
+        if hasproperty(node, :args)
+            new_args = map(node.args) do c
+                if c isa LineNumberNode
+                    LineNumberNode(current_line + c.line - 1, file)
+                else
+                    c
+                end
+            end
+            node.args = new_args
+        end
+    end
+    @debug "expr after adjustment" expr
+
+    local repl_response
+    try
+        repl_response = Pair{Any, Bool}(Base.eval(session.evaluatein, expr), false)
+    catch exc
+        repl_response = Pair{Any, Bool}(current_exceptions(), true)
+        @debug "Got an error" exc stacktrace(Base.catch_backtrace())
+        stack = stacktrace(Base.catch_backtrace())
+        put!(session.responsechannel, Protocols.Error(msgid, exc, stack))
+    end
+    setglobal!(Base.MainInclude, :ans, first(repl_response))
+    hide_output = REPL.ends_with_semicolon(eval_string)
+    REPL.print_response(repl, repl_response, !hide_output, REPL.hascolor(repl))
+    REPL.LineEdit.reset_state(s)
+    REPL.LineEdit.refresh_line(s)
 end
